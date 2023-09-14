a536ddecaac47f42c5c18814e2b2d59f78d5c454
I5 <I5@WIN-B4QQO3M5EUJ>
2023-09-14 22:02:18 +0200

    Добавьте файлы проекта.

---
+++ lab1Popov/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите y:");
+double y = double.Parse(Console.ReadLine());
+Console.Write("Введите n:");
+double n = double.Parse(Console.ReadLine());
+Console.Write("Введите D:");
+double D = double.Parse(Console.ReadLine());
+Console.WriteLine(D = y * y * ((0.5 * n + 4.8) / (Math.Sin(y))));
+Console.WriteLine($"D={D:F2}");
