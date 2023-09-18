9147e45480cd1f7eef1f749052da6b3049f72fc4
ArtemPpop <144240156+ArtemPpop@users.noreply.github.com>
2023-09-18 12:35:46 +0200

    Program.cs

--- lab1Popov/Program.cs
+++ lab1Popov/Program.cs
@@ -1,8 +1,6 @@
-//средний 11
 Console.Write("Введите y:");
 double y = double.Parse(Console.ReadLine());
 Console.Write("Введите n:");
 double n = double.Parse(Console.ReadLine());
-Console.Write("Введите D:");
 double D = D = y * y * ((0.5 * n + 4.8) / (Math.Sin(y)));
 Console.WriteLine($"{D:F2}");
